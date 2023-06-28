727d6960b793075de1bcae0a718a60ccdf05246d
Nandini-Das <nandini.rima181@gmail.com>
2023-06-28 16:39:04 +0600

    add

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -81,11 +81,7 @@ const AddProduct = () => {
             .catch((error) => {
 
                 console.error('Error submitting form data:', error.response);
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Failed to add product',
-                });
+                
             });
 
     };
